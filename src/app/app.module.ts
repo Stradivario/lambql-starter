50c9eb0ef957155290ab0c7bed9e070703813d20
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2025-12-25 19:26:00 +0200

    fix(gapi): bumped version of gapi and removed many dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { MongoDbModule } from '@core/mongo';
 import { CoreModule, Module } from '@gapi/core';
 import { GraphQLCustomDirective } from '@gapi/core';
 import { VoyagerModule } from '@gapi/voyager';
+import { AltairModule } from '@rxdi/altair';
 import { SharedModule } from '@shared/shared.module';
 import { DirectiveLocation } from 'graphql';
 
-import { ENVIRONMENT, isDevelopment } from '~/environment';
+import { ENVIRONMENT } from '~/environment';
 
 import { AppInterceptors } from './app.interceptors';
 import { UserModule } from './user/user.module';
@@ -26,25 +27,6 @@ export const ToUpperCaseDirective = new GraphQLCustomDirective<string>({
       graphql: {
         directives: [ToUpperCaseDirective],
         path: '/graphql',
-        initQuery: true,
-        openBrowser: false,
-        writeEffects: false,
-        graphiql: false,
-        graphiQlPlayground: true,
-        graphiQlPath: '/graphiql',
-        graphiqlOptions: {
-          endpointURL: '/graphql',
-          subscriptionsEndpoint: `${process.env.GRAPHIQL_WS_SSH ? 'wss' : 'ws'}://${
-            process.env.GRAPHIQL_WS_PATH || 'localhost'
-          }:${process.env.API_PORT || process.env.PORT || 9000}/subscriptions`,
-          websocketConnectionParams: {
-            token: process.env.GRAPHIQL_TOKEN,
-          },
-        },
-        graphqlOptions: {
-          tracing: isDevelopment(),
-          schema: null,
-        },
       },
       server: {
         hapi: {
@@ -73,6 +55,7 @@ export const ToUpperCaseDirective = new GraphQLCustomDirective<string>({
     }),
     MongoDbModule.forRoot(ENVIRONMENT.MONGODB_URI),
     VoyagerModule.forRoot(),
+    AltairModule.forRoot(),
     SharedModule,
     AppInterceptors.forRoot(),
     UserModule,
